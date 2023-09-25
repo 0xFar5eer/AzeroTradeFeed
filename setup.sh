2a91075616c37347c77a05254b6d0309c5d586b8
0xFar5eer <0xfar5eer@gmail.com>
2023-09-25 16:59:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -223,6 +223,5 @@ sudo usermod -aG docker user;
 
 # github runner
 cd ~; mkdir actions-runner && cd actions-runner; curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz; tar xzf ./actions-runner-linux-x64-2.309.0.tar.gz; 
-./config.sh --url https://github.com/0xFar5eer/AzeroTradeFeed --token XXXXXXX
+./config.sh --url https://github.com/0xFar5eer/AzeroTradeFeed --token AZRKNYZDBOVNXXFU7HZC2ULFCFARC
 sudo ./svc.sh install; sudo ./svc.sh start
-
